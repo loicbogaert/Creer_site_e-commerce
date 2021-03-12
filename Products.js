f6691bed6671beedd32e6a1b83df339e4c37d91b
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-03-12 23:29:23 +0100

    tentative de changement de l'url

--- Products.js
+++ Products.js
@@ -4,7 +4,12 @@
 /** voir async await à la place des promise */
 /*get id (prendre id de la page) (voir liens)*/
 
- 
+
+/**modification d'une url (failed) */
+let url = new URL ('http://orinoco/Home.html');
+let params = new URLSearchParams(url.search.slice(1));
+
+params.append('ffffff', 4);
 
         /*Array avec les données de l'API*/
 
@@ -20,50 +25,55 @@ ServerData().then(json =>{
     var product = json;
     console.log(product);
 
-    /** */
+
+    /*Boucle qui s'active pour chaque élément du tableau Product contenant les objets de l'api*/
 
     for (let i = 0; i < product.length; i++) {
+
+    /*Création des différentes div (1 div = un produit) + ajout classe / id*/
+
     var divSquare = document.createElement("div")
     divSquare.className = 'main__square';
     divSquare.id = 'mainSquare' + [i];
 
 
-    /*bootstrap classes*/
-divSquare.classList.add("col-9");
-divSquare.classList.add("col-md-5");
-divSquare.classList.add("col-md-5");
-divSquare.classList.add("col-lg-3");
+    /*Ajout des classes bootstrap*/
+
+    divSquare.classList.add("col-9");
+    divSquare.classList.add("col-md-5");
+    divSquare.classList.add("col-md-5");
+    divSquare.classList.add("col-lg-3");
 
 
 /**création des éléments + noms de classe*/
 
-var divImg = document.createElement("div");
-divImg.classList.add("main__img");
+    var divImg = document.createElement("div");
+    divImg.classList.add("main__img");
 
-var div = document.createElement("h2");
-div.classList.add ("main__title");
+    var div = document.createElement("h2");
+    div.classList.add ("main__title");
 
-var div2 = document.createElement("p");
-div2.classList.add ("main__price");
+    var div2 = document.createElement("p");
+    div2.classList.add ("main__price");
 
-var div3 = document.createElement("p");
-div3.classList.add ("main__description");
+    var div3 = document.createElement("p");
+    div3.classList.add ("main__description");
 
-var div4 = document.createElement("p");
-div4.classList.add ("main__code");
+    var div4 = document.createElement("p");
+    div4.classList.add ("main__code");
 
     /*Création des éléments de texte*/
 
-var id = document.createTextNode ("product reference : " + product[i]._id);
-var name = document.createTextNode(product[i].name);
-var price = document.createTextNode(product[i].price/100 + " $");
-var description = document.createTextNode(product[i].description);
+    var id = document.createTextNode ("product reference : " + product[i]._id);
+    var name = document.createTextNode(product[i].name);
+    var price = document.createTextNode(product[i].price/100 + " $");
+    var description = document.createTextNode(product[i].description);
         
 
     /*Céation des images*/
 
-var image = document.createElement("img");
-image.src = product[i].imageUrl;
+    var image = document.createElement("img");
+    image.src = product[i].imageUrl;
 
 
       /*Ajout de tout les éléments "product" aux sous-div du MAIN*/ 
@@ -102,5 +112,20 @@ image.src = product[i].imageUrl;
             }
  }
  })
-                        
-        
+
+
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
