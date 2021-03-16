7e11e57cb89cb25b2c674d934825a370c85ee6c7
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-03-16 15:18:17 +0100

    boutons pour les choix de lentilles remplacés par des input radio (next step : écouter ces input)

--- Products.js
+++ Products.js
@@ -49,13 +49,19 @@ function $_GET(param) {
                 var image2 = document.createElement("img");
                 var description2  = document.createElement("p");
                 var lenses = document.createElement("div");
+
+                var lensesChoice =  document.createElement("p");
+                var createChoice =  document.createTextNode("Please choose your lense type :");
+                lensesChoice.appendChild(createChoice);
                
                 /**Ajout des éléments à la div principale */
                 page2.appendChild(title2);
                 page2.appendChild(image2);
                 page2.appendChild(description2);
                 page2.appendChild(price2);
+                page2.appendChild(lensesChoice);
                 page2.appendChild(lenses);
+                
 
                 console.log(page2);
 
@@ -67,32 +73,42 @@ function $_GET(param) {
                     
     
                 /**Ajout des textes/images créés à leur div respective */
+
                         title2.appendChild(name);
                         price2.appendChild(price);
                         description2.appendChild(description);
                         image2.src = product[item].imageUrl;
 
-                /**Ajout des différentes options d'objectif */
+
+                /**Ajout des différentes options de lentille */
                 var lense = product[item].lenses;
                 for (let i = 0; i < lense.length; i++) {
-                   var buttonLense = document.createElement("button");
-                   lenses.appendChild(buttonLense);
+                   var buttonLense = document.createElement("input");
+                   buttonLense.setAttribute("type", "radio");
+                   buttonLense.setAttribute("name", "lense");
+                   buttonLense.classList.add("productPage__checkbox");
+
+                   var choiceContainer = document.createElement("div");
+
+                   var lensesLabel = document.createElement("label");
+                   lenses.appendChild(lensesLabel);
+
                    var lensesText = document.createTextNode(lense[i]);
-                   buttonLense.appendChild(lensesText);
+                   lensesLabel.appendChild(lensesText);
+
+                   choiceContainer.appendChild(buttonLense);
+                   choiceContainer.appendChild(lensesLabel);
+                   lenses.appendChild(choiceContainer);
                 }
 
                 /**Création de classes */
 
                 page2.classList.add("productPage");
-
                 image2.classList.add("productPage__img");
-
                 title2.classList.add("productPage__title", "bold");
-
                 price2.classList.add("productPage__price", "bold");
-
                 description2.classList.add("productPage__description");
-
+                lensesChoice.classList.add("productPage__lenseType");
 
             })
         }
